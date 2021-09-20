d6588ce017f8f809251ef6b39001a6ccbfc0a378
Roly Perera <roly.perera@dynamicaspects.org>
2021-09-20 13:39:02 +0100

    ♻️ : More basic structure; ways package.

---
+++ ways/__init__.py
@@ -0,0 +1,10 @@
+"""ways namespace."""
+
+from importlib_metadata import PackageNotFoundError, version
+
+# Used to automatically set version number from github actions
+# as well as not break when being tested locally
+try:
+    __version__ = version(__package__)  # type: ignore
+except PackageNotFoundError:  # pragma: no cover
+    __version__ = "0.0.0"
