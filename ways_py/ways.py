2a68e8035274dff3367d1bb84d73f79214307d59
Roly Perera <roly.perera@dynamicaspects.org>
2021-09-21 08:37:21 +0100

    ♻️ : Add flake8 docstrings, import-order and colors subpackages.

--- ways_py/ways.py
+++ ways_py/ways.py
@@ -1,2 +1,10 @@
+import altair as alt
+import pandas as pd
+
+
 class Ways:
     """The WAYS library."""
+
+    def dummy_chart(self) -> alt.Chart:
+        df: pd.DataFrame = pd.DataFrame(columns=["x", "y"])
+        return alt.Chart(df, title="Mean Control Chart").properties(width=600, height=500)
