20e4efc19b24f008da361888a78f1e8b467d0571
Ed Chalstrey <edwardchalstrey@gmail.com>
2021-09-27 16:12:12 +0100

    ✨ : restructure altair_meta_hist return

--- ways_py/ways.py
+++ ways_py/ways.py
@@ -23,11 +23,10 @@ class Ways:
             altair chart object: histogram
         """
         return alt.Chart(dataframe).mark_bar().encode(
-                alt.X(column, bin=bin),
-                y='count()',
-               ).encode(
-                color
-               ).properties(
-                width=300,
-                height=300
-               )
+            y='count()',
+        ).encode(
+            color
+        ).properties(
+            width=300,
+            height=300
+        )
