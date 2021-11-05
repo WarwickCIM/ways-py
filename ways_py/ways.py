b562173f6354c22dd16eabcde8c124b6369301be
Roly Perera <roly.perera@dynamicaspects.org>
2021-11-05 07:47:05 +0000

    ♻️ : Extract colour_bars, density_chart methods.

--- ways_py/ways.py
+++ ways_py/ways.py
@@ -8,17 +8,8 @@ class Ways:
     """WAYS library."""
 
     @staticmethod
-    def altair_meta_hist(src: alt.Chart) -> alt.Chart:
-        """Altair metavisualisation; histogram visualising color bins of another Altair chart.
-
-        Args:
-        src: colour-encoded Altair chart underlying the metavisualisation.
-        str: column of source chart's data which contains the colour-encoded data.
-
-        Returns:
-            altair chart object: histogram
-        """
-        density_chart = alt.Chart(src.data) \
+    def density_chart(src: alt.Chart) -> alt.Chart:
+        return alt.Chart(src.data) \
             .transform_joinaggregate(total='count(*)') \
             .transform_calculate(proportion="1 / datum.total") \
             .mark_bar(color='gray') \
@@ -33,9 +24,12 @@ class Ways:
             ) \
             .properties(width=300, height=300)
 
+
+    @staticmethod
+    def colour_bars(src: alt.Chart) -> alt.Chart:
         y_axis = alt.Axis(orient='right', labels=False, tickSize=0, grid=False)
         x_axis = alt.Axis(labels=False, tickSize=0, grid=False)
-        colour_bars = alt.Chart(src.data) \
+        return alt.Chart(src.data) \
             .mark_rect() \
             .transform_bin(as_=['y', 'y2'], bin=src.encoding.color.bin, field='pct_estimate') \
             .encode(
@@ -45,7 +39,20 @@ class Ways:
             ) \
             .encode(src.encoding.color)
 
-        return density_chart | colour_bars | src
+
+    @staticmethod
+    def altair_meta_hist(src: alt.Chart) -> alt.Chart:
+        """Altair metavisualisation; histogram visualising color bins of another Altair chart.
+
+        Args:
+        src: colour-encoded Altair chart underlying the metavisualisation.
+        str: column of source chart's data which contains the colour-encoded data.
+
+        Returns:
+            altair chart object: histogram
+        """
+
+        return Ways.density_chart(src) | Ways.colour_bars(src) | src
 
 
 FuncT = TypeVar("FuncT", bound=Callable[..., Any])
