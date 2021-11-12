8f72bcee515b442d2e4282f3a8bf59238b1dcdde
Ed Chalstrey <edwardchalstrey@gmail.com>
2021-11-12 16:52:37 +0000

    🐛 : stop y axis sharing with main plot

--- ways_py/ways.py
+++ ways_py/ways.py
@@ -71,8 +71,10 @@ class Ways:
         Returns:
             Altair chart object: modified chart
         """
-        meta_chart: alt.Chart = (Ways.density_chart(src) | Ways.used_colours(src)).resolve_scale(y='shared')
-        return (meta_chart | src) \
+        def meta_chart(src: alt.Chart) -> alt.Chart:
+            """Create a density chart with used colors plot that share a y axis."""
+            return Ways.density_chart(src) | Ways.used_colours(src).resolve_scale(y='shared')
+        return (meta_chart(src) | src) \
             .configure_view(strokeWidth=0) \
             .configure_concat(spacing=5)
 
