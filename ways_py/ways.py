99ee70b48d1a7586ac11c834d99b8a91b1f84643
Roly Perera <roly.perera@dynamicaspects.org>
2021-11-18 09:54:23 +0000

    ♻️ : Separate encoding.color.bin cases from non-bin case.

--- ways_py/ways.py
+++ ways_py/ways.py
@@ -53,17 +53,27 @@ class Ways:
         else:
             y_scale = alt.Scale(zero=False)
         x_axis = alt.Axis(labels=False, tickSize=0, grid=False, titleAngle=270, titleAlign='right')
-        chart = alt.Chart(src.data).mark_rect()
         if src.encoding.color.bin:
+            chart = alt.Chart(src.data).mark_rect()
             chart = chart.transform_bin(as_=['y', 'y2'], bin=src.encoding.color.bin, field=Ways.field(src))
-        return chart.transform_calculate(x='5') \
-            .encode(
-                y=alt.Y('y:Q', scale=y_scale, axis=y_axis, title=""),
-                y2='y2:Q',
-                x=alt.X('x:Q', sort='descending', axis=x_axis, title="colours used")
-            ) \
-            .encode(src.encoding.color) \
-            .properties(width=20, height=300)  # noqa: E123
+            return chart.transform_calculate(x='5') \
+                .encode(
+                    y=alt.Y('y:Q', scale=y_scale, axis=y_axis, title=""),
+                    y2='y2:Q',
+                    x=alt.X('x:Q', sort='descending', axis=x_axis, title="colours used")
+                ) \
+                .encode(src.encoding.color) \
+                .properties(width=20, height=300)  # noqa: E123
+        else:
+            chart = alt.Chart(src.data).mark_rect()
+            return chart.transform_calculate(x='5') \
+                .encode(
+                    y=alt.Y('y:Q', scale=y_scale, axis=y_axis, title=""),
+                    y2='y2:Q',
+                    x=alt.X('x:Q', sort='descending', axis=x_axis, title="colours used")
+                ) \
+                .encode(src.encoding.color) \
+                .properties(width=20, height=300)  # noqa: E123
 
     @staticmethod
     def altair_meta_hist(src: alt.Chart) -> alt.Chart:
